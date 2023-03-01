f5ca014519eeb76dc90b9b159b78cdf4dd150ad3
Graham Palmer <33918961+grahampalmer@users.noreply.github.com>
2023-03-01 21:46:04 +1100

    Delete code_BARRA/BARRA-TA directory

--- code_BARRA/BARRA-TA/find.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-python find.py -40.7740 144.7339 80 WOOLNTH1 2018
-python find.py -40.7750 148.0000 80 MUSSELR1 2018
-python find.py -45.151	146.688	110 CTHLWF1 2018
-python find.py -41.802	145.056 137 GRANWF1	2018
-
-
-
